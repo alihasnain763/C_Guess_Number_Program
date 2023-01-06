52b80b6196b80706d069008fddc7a3c62d094fd8
Ali Hasnain <abdulrehman76261@gmail.com>
2023-01-05 23:12:18 -0800

    complete c program/game guess correct number

---
+++ game.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int main(){
+
+    int number, guess, nguesses;
+        srand(time(0));
+        number = rand()% 100 + 1;
+        // printf("The number is %d ", number);
+        // keep running the loop until the number is gussed
+            do{
+                printf("Guess number between 1 to 100\n");
+                scanf("%d", &guess);
+                if(guess>number){
+                    printf("Lower number please \n");
+                }
+                else if(guess<number){
+                    printf("Higher number please! \n");
+                }
+                else{
+                    printf("you guessed itin %d attempts \n", nguesses );
+                }
+                nguesses++;
+            }while(guess != number);
+    return 0;
+}
